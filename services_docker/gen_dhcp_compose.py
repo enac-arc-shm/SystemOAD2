69712abf699c52bcc8506d119ca28be8cebfa053
anasn-uttt <21300042@uttt.edu.mx>
2023-03-16 10:37:04 -0600

    new

--- services_docker/gen_dhcp_compose.py
+++ services_docker/gen_dhcp_compose.py
@@ -2,7 +2,7 @@
 import yaml
 import os
 
-#recibe como parámetros la dirección de subred, el primer y último valor del rango de direcciones IP ("range_start" y "range_end", respectivamente)
+#recibe como parámetros la dirección de subred, el primer y último valor del rango de direcciones IP ("range_start" y "range_end")
 def generate_dhcp_compose(subnet, range_start, range_end):
     compose_data = {
         "version": "3",
@@ -24,5 +24,5 @@ def generate_dhcp_compose(subnet, range_start, range_end):
 
     with open("docker-compose.yml", "w") as compose_file:
         yaml.dump(compose_data, compose_file)
-
+# levantar el servicio DHCP
     os.system("docker-compose up -d")
