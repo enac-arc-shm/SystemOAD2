0fda3260878378ede87b2d1ed1d0b3f9c8cc5f6e
anasn-uttt <21300042@uttt.edu.mx>
2023-04-08 16:46:37 -0600

    V1.3

--- services_docker/DHCP/app.py
+++ services_docker/DHCP/app.py
@@ -91,35 +91,6 @@ def validate_ip_address(ip):
 def index():
     return render_template('index.html')
 
-
-# @app.route('/generate-dhcp', methods=['POST'])
-# def generate_dhcp():
-#    subnet = request.form["subnet"]
-#    gateway = request.form["gateway"]
-#    start_ip = request.form["start_ip"]
-#    end_ip = request.form["end_ip"]
-#    subnet_mask = request.form["subnet_mask"]
-
-#    with open("dhcpd.conf.template", "r") as f:
-#        template = f.read()
-#        dhcpd_conf = template.replace("{{ subnet }}", subnet).replace(
-#            "{{ gateway }}", gateway).replace("{{ start_ip }}", start_ip).replace("{{ end_ip }}", end_ip).replace("{{ subnet_mask }}", subnet_mask)
-#    with open("dhcpd.conf", "w") as f:
-#        f.write(dhcpd_conf)
-
-#    with open("docker-compose.template.yml", "r") as f:
-#        template = f.read()
-#        docker_compose = template.replace("{{ subnet }}", subnet).replace(
-#            "{{ gateway }}", gateway).replace("{{ start_ip }}", start_ip).replace("{{ end_ip }}", end_ip).replace("{{ subnet_mask }}", subnet_mask)
-#    with open("docker-compose.yml", "w") as f:
-#        f.write(docker_compose)
-
-#    subprocess.run(['docker', 'build', '-t', 'networkboot/dhcpd:latest', '.'])
-#    subprocess.run(['docker-compose', '-f', 'docker-compose.yml', 'up', '-d',
-#                   '--force-recreate'], cwd='.', stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#    subprocess.run(['docker-compose', '-f', 'docker-compose.yml', '-v', '$(pwd)/data:/data', 'up', '-d', '--force-recreate'])
-#    return "DHCP server started successfully!"
-
 @app.route('/generate-dhcp', methods=['POST'])
 def generate_dhcp():
     subnet = request.form["subnet"]
